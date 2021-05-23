270c54d83500feeca1fd79c2a599e90e62391a4a
Ryan Riley Puzon <rrgpuzon@gmail.com>
2021-05-22 20:13:21 -0700

    setting up enzyme

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,14 @@
-import { render, screen } from '@testing-library/react';
+import { configure, render, screen } from '@testing-library/react';
+import { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+
+describe('App', ()=> {
+  let wrapper;
+  beforeAll(() => {
+    wrapper = shallow(<App />);
+  })
+  test('renders learn react link', () => {
+  });
+})
