20a547050d7379354fd4f86d550faa2ed2d27456
Ryan Riley Puzon <rrgpuzon@gmail.com>
2021-05-22 20:36:00 -0700

    creating first test

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import logo from './logo.svg';
+import {useState} from 'react';
 import './App.css';
 
 function App() {
+  const [count, setCount] = useState(0);
   return (
     <div className="App">
     </div>
--- src/App.test.js
+++ src/App.test.js
@@ -4,11 +4,12 @@ import Adapter from 'enzyme-adapter-react-16';
 import App from './App';
 
 
-describe('App', ()=> {
+describe('App Test Suite', ()=> {
   let wrapper;
   beforeAll(() => {
     wrapper = shallow(<App />);
   })
-  test('renders learn react link', () => {
+  test('renders with no errors', () => {
+    expect(shallow(<App />).contains(<div className='App'></div>)).toBe(true);
   });
 })
